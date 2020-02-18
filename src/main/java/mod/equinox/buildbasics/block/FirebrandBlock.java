e72bcd6bf0e713e8196be21bdd81ffe1f3e71138
equinoxrex <58395866+equinoxrex@users.noreply.github.com>
2020-02-18 16:56:54 +0100

    Not much

--- src/main/java/mod/equinox/buildbasics/block/FirebrandBlock.java
+++ src/main/java/mod/equinox/buildbasics/block/FirebrandBlock.java
@@ -108,12 +108,12 @@ public class FirebrandBlock extends Block {
     /*@OnlyIn(Dist.CLIENT)
     public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
         Direction direction = stateIn.get(FACING);
-        double d0 = (double)pos.getX() + 0.55D - (double)(rand.nextFloat() * 0.1F);
-        double d1 = (double)pos.getY() + 0.55D - (double)(rand.nextFloat() * 0.1F);
-        double d2 = (double)pos.getZ() + 0.55D - (double)(rand.nextFloat() * 0.1F);
+        double d0 = (double)pos.getX() + 0.5D - (double)(rand.nextFloat() * 0.1F);
+        double d1 = (double)pos.getY() + 0.5D - (double)(rand.nextFloat() * 0.1F);
+        double d2 = (double)pos.getZ() + 0.5D - (double)(rand.nextFloat() * 0.1F);
         double d3 = (double)(0.4F - (rand.nextFloat() + rand.nextFloat()) * 0.4F);
         if (rand.nextInt(5) == 0) {
-            worldIn.addParticle(ParticleTypes.END_ROD, d0 + (double)direction.getXOffset() * d3, d1 + (double)direction.getYOffset() * d3, d2 + (double)direction.getZOffset() * d3, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D);
+            worldIn.addParticle(ParticleTypes.FLAME, d0 + (double)direction.getXOffset() * d3, d1 + (double)direction.getYOffset() * d3, d2 + (double)direction.getZOffset() * d3, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D);
         }
 
     }*/
